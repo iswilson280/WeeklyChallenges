4c7343cbcb9222f3af0501be7c557dcc3a0ac8b8
Isaiah Wilson <isaiahrwilson@gmail.com>
2023-08-14 15:28:29 -0500

    weekly challenges

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,44 +6,70 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
+
+
+            if (num1 == num2)
             {
                 return true;
             }
+
             else
             {
                 return false;
             }
+
         }
+
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+
+            double ans = minuend - subtrahend;
+
+            return ans;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int ans = number1 + number2;
+
+            return ans;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long ans = factor1 * factor2;
+
+            return ans;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+
+
+            return nameOfPerson == "" ? "Hello!" : $"Hello, {nameOfPerson}!";
+
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+
+            string x = "HEY!";
+            return x;
         }
     }
 }
